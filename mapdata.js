d6802c683808ec9f052ddd84284ea2e21cc60e05
devinerickson15 <devinjohn15@gmail.com>
2017-07-25 16:54:42 -0400

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,367 @@
+var simplemaps_usmap_mapdata={
+  main_settings: {
+    //General settings
+    width: "responsive", //or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    
+    //Label defaults
+    label_color: "#333333",
+    hide_labels: "no",
+    border_color: "#a3c2de",
+    state_description: "",
+    state_color: "#3d85c5",
+    state_hover_color: "#ff9c39",
+    state_url: "",
+    all_states_inactive: "no",
+    
+    //Location defaults
+    location_description: "Field Office",
+    location_color: "hotpink",
+    location_opacity: ".9",
+    location_url: "http://simplemaps.com",
+    all_states_zoomable: "yes",
+    location_size: "20",
+    location_type: "circle",
+    all_locations_inactive: "no",
+    url_new_tab: "yes",
+    auto_load: "yes",
+   
+    //Zoom settings
+    zoom: "yes",
+    initial_zoom: "-1",
+    initial_zoom_solo: "yes",
+    popups: "detect",
+    location_image_url: "",
+    border_size: ".75",
+    label_size: "0"
+  },
+  state_specific: {
+    HI: {
+      name: "Hawaii",
+      description: "default",
+      color: "#cac6c1",
+      hover_color: "default",
+      url: "default",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "default"
+    },
+    AK: {
+      name: "Alaska",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    FL: {
+      name: "Florida",
+      inactive: "no",
+      hide: "no"
+    },
+    NH: {
+      name: "New Hampshire",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    VT: {
+      name: "Vermont",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    ME: {
+      name: "Maine",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    RI: {
+      name: "Rhode Island",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    NY: {
+      name: "New York",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    PA: {
+      name: "Pennsylvania",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    NJ: {
+      name: "New Jersey",
+      hide: "no",
+      inactive: "no"
+    },
+    DE: {
+      name: "Delaware",
+      hide: "no",
+      inactive: "no"
+    },
+    MD: {
+      name: "Maryland",
+      hide: "no",
+      inactive: "no"
+    },
+    VA: {
+      name: "Virginia",
+      hide: "no",
+      inactive: "no"
+    },
+    WV: {
+      name: "West Virginia",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    OH: {
+      name: "Ohio",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    IN: {
+      name: "Indiana",
+      hide: "no",
+      inactive: "no"
+    },
+    IL: {
+      name: "Illinois",
+      hide: "no",
+      inactive: "no"
+    },
+    CT: {
+      name: "Connecticut",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    WI: {
+      name: "Wisconsin",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    NC: {
+      name: "North Carolina",
+      hide: "no",
+      inactive: "no",
+      color: "#a61c01"
+    },
+    DC: {
+      name: "District of Columbia",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    MA: {
+      name: "Massachusetts",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    TN: {
+      name: "Tennessee",
+      hide: "no",
+      inactive: "no",
+      color: "#a61c01"
+    },
+    AR: {
+      name: "Arkansas",
+      hide: "no",
+      inactive: "no"
+    },
+    MO: {
+      name: "Missouri",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    GA: {
+      name: "Georgia",
+      hide: "no",
+      inactive: "no",
+      color: "#a61c01"
+    },
+    SC: {
+      name: "South Carolina",
+      hide: "no",
+      inactive: "no"
+    },
+    KY: {
+      name: "Kentucky",
+      hide: "no",
+      inactive: "no",
+      color: "#a61c01"
+    },
+    AL: {
+      name: "Alabama",
+      hide: "no",
+      inactive: "no"
+    },
+    LA: {
+      name: "Louisiana",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    MS: {
+      name: "Mississippi",
+      hide: "no",
+      inactive: "no"
+    },
+    IA: {
+      name: "Iowa",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    MN: {
+      name: "Minnesota",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    OK: {
+      name: "Oklahoma",
+      hide: "no",
+      inactive: "no"
+    },
+    TX: {
+      name: "Texas",
+      hide: "no",
+      inactive: "no"
+    },
+    NM: {
+      name: "New Mexico",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    KS: {
+      name: "Kansas",
+      hide: "no",
+      inactive: "no"
+    },
+    NE: {
+      name: "Nebraska",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    SD: {
+      name: "South Dakota",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    ND: {
+      name: "North Dakota",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    WY: {
+      name: "Wyoming",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    MT: {
+      name: "Montana",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    CO: {
+      name: "Colorado",
+      hide: "no",
+      inactive: "no"
+    },
+    UT: {
+      name: "Utah",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    AZ: {
+      name: "Arizona",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    NV: {
+      name: "Nevada",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    OR: {
+      name: "Oregon",
+      hide: "no",
+      inactive: "no"
+    },
+    WA: {
+      name: "Washington",
+      hide: "no",
+      inactive: "no"
+    },
+    CA: {
+      name: "California",
+      hide: "no",
+      inactive: "no"
+    },
+    MI: {
+      name: "Michigan",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    ID: {
+      name: "Idaho",
+      hide: "no",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    GU: {
+      name: "Guam",
+      hide: "yes",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    VI: {
+      name: "Virgin Islands",
+      hide: "yes",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    PR: {
+      name: "Puerto Rico",
+      hide: "yes",
+      inactive: "yes",
+      color: "#cac6c1"
+    },
+    MP: {
+      name: "Northern Mariana Islands",
+      hide: "yes",
+      inactive: "yes",
+      color: "#cac6c1"
+    }
+  },
+  locations: {},
+  regions: {},
+  continent: [
+    {
+      x: "0",
+      y: "0",
+      height: "615.7894736848264",
+      width: "1000"
+    }
+  ]
+};
